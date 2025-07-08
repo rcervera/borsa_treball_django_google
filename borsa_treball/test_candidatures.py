ccb7d8731469968880da81fa6b592db7370c7814
Ramon Cervera <rcerver4@xtec.cat>
2025-07-08 14:07:44 +0200

    test candidatures

--- borsa_treball/test_candidatures.py
+++ borsa_treball/test_candidatures.py
@@ -92,6 +92,7 @@ class AfegirCandidaturaTest(TestCase):
       
         response = self.client.post(url, dades, content_type='multipart/form-data')
       
+        print("JSON response:", response.json())
 
         self.assertEqual(response.status_code, 201)
         self.assertEqual(Candidatura.objects.count(), 1)
