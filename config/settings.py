5333f0b2fc5a17d1f2ddea3c4b67021dbd7fca21
Ramon Cervera <rcerver4@xtec.cat>
2025-09-16 15:40:48 +0200

    send new offer  email

--- config/settings.py
+++ config/settings.py
@@ -224,5 +224,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'borsatreballvidal@gmail.com' 
-EMAIL_HOST_PASSWORD = 'saca kgvv nvtk apzp' 
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD') 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
