cf8ce648f6a38d44c504d5a40b1e313906872cdc
Ramon Cervera <rcerver4@xtec.cat>
2025-09-16 15:16:05 +0200

    send new offer  email

--- config/settings.py
+++ config/settings.py
@@ -223,5 +223,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'borsatreball@vidalibarraquer.net' 
+EMAIL_HOST_USER = 'rcerver4@xtec.cat' 
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  
