db9f479338ed256719ed6e4740f2ad46859d01cd
Ramon Cervera <rcerver4@xtec.cat>
2025-09-21 18:38:35 +0200

    send email canvi estat candidatura

--- borsa_treball/signals.py
+++ borsa_treball/signals.py
@@ -23,7 +23,7 @@ def enviar_email_si_activa(sender, instance, created, **kwargs):
     RegistreAuditoria.objects.create(
                     accio="Activació Candidatura",
                     model_afectat="Candidatura",
-                    descripcio=f"Candidatura {instance.pk} activada per l'estudiant {instance.estudiant.get_full_name()} a l'oferta {instance.oferta}.",
+                    descripcio=f"Candidatura {instance.pk} activada per l'estudiant {instance.estudiant.usuari.get_full_name()} a l'oferta {instance.oferta}.",
                     usuari=None,  # Opcional: si vols, posa l'usuari actual si tens request
                     data=timezone.now(),
                     revisat=False
