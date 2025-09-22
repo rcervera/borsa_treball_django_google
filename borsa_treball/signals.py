876784e95c51e0295956c227fcef85673bffd66e
Ramon Cervera <rcerver4@xtec.cat>
2025-09-22 10:38:22 +0200

    send email canvi estat candidatura

--- borsa_treball/signals.py
+++ borsa_treball/signals.py
@@ -1,37 +1,34 @@
-# borsa_treball/signals.py
 import logging
-from django.db.models.signals import post_save
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from django.core.mail import EmailMultiAlternatives, mail_admins
 from django.core.mail import send_mail
-from django.template.loader import render_to_string
-from django.urls import reverse
 from django.conf import settings
 from .models import Candidatura
 
 logger = logging.getLogger('borsa_treball')
 
-@receiver(post_save, sender=Candidatura)
-def enviar_email_si_activa(sender, instance, created, **kwargs):
+@receiver(pre_save, sender=Candidatura)
+def enviar_email_si_activa(sender, instance, **kwargs):
     """
-    Envia un email a l'usuari de l'empresa quan una candidatura passa de activa=False a activa=True.
-    Si hi ha error, envia un email als admins i registra l'error.
+    Signal de prova: envia un email senzill a rcerver4@xtec.cat
+    quan el camp activa passa de False a True.
     """
-    """
-    Signal de prova: envia un email senzill a rcerver4@xtec.cat quan activa passa a True
-    """
-    if not created:
+    # només si l'objecte ja existeix (no és una creació)
+    if instance.pk:
         try:
-            
-            if instance.activa:
-                    
-                    subject = "Prova d'email: Candidatura activada"
-                    message = f"La candidatura {instance.pk} de l'estudiant {instance.estudiant.usuari.get_full_name()}  s'ha activat."             
-                    to = ['rcerver4@xtec.cat']
+            old_instance = sender.objects.get(pk=instance.pk)
+            # comprovem el canvi d'estat
+            if not old_instance.activa and instance.activa:
+                subject = "Prova d'email: Candidatura activada"
+                message = (
+                    f"La candidatura {instance.pk} "
+                    f"de l'estudiant {instance.estudiant.usuari.get_full_name()} "
+                    f"s'ha activat."
+                )
+                to = ['rcerver4@xtec.cat']
 
-                    send_mail(subject, message,None, to)
-                    
+                send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, to)
+
         except Exception as e:
-                error_msg = f"Error enviant email de prova per candidatura {instance.pk}: {e}"
-                logger.error(error_msg)
-                
+            error_msg = f"Error enviant email de prova per candidatura {instance.pk}: {e}"
+            logger.error(error_msg)
