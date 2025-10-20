853ad18984a190bccd1da33fe2029c1dd620fbc3
Ramon Cervera <rcerver4@xtec.cat>
2025-10-20 12:20:14 +0200

    Admin panel: afegit botó enviar email estudiants oferta

--- borsa_treball/tasks.py
+++ borsa_treball/tasks.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 from django.core.mail import EmailMultiAlternatives
 
 @task()
-def enviar_email_async(subject, message, destinatari_list, html_message, bcc_list=None, _dummy=None):
+def enviar_email_async(subject, message, destinatari_list, html_message, bcc_list, _dummy):
     """
     Envia un email asíncronament amb possibilitat de BCC.
     """
