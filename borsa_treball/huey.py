30c4f7089a11982342fb84b4b7829a14a500e00b
Ramon Cervera <rcerver4@xtec.cat>
2025-10-23 18:08:34 +0200

    Admin panel: afegit botó enviar email estudiants oferta

--- borsa_treball/huey.py
+++ borsa_treball/huey.py
@@ -1,6 +1,7 @@
 from huey import RedisHuey
 from django.conf import settings
 
+# Crear la instància Huey basant-nos en la configuració del settings.py
 huey = RedisHuey(
     name=settings.HUEY.get('name', 'borsa_treball'),
     host=settings.HUEY.get('host', '127.0.0.1'),
