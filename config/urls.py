b652b60f806943208ea819ddc65fbd1fd6737bc3
Ramon Cervera <rcerver4@xtec.cat>
2026-03-06 15:14:11 +0100

    reset password

--- config/urls.py
+++ config/urls.py
@@ -27,7 +27,8 @@ urlpatterns = [
     path('auth/', include('social_django.urls', namespace='social')),  # Login social
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 
-    path("password-reset/", auth_views.PasswordResetView.as_view(template_name="registration/password_reset_form.html"), name="password_reset"),
+   
+    path("password-reset/", auth_views.PasswordResetView.as_view(template_name="registration/password_reset_form.html",  email_template_name="registration/password_reset_email.html"), name="password_reset"),
     path("password-reset/done/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
     path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path("reset/done/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
